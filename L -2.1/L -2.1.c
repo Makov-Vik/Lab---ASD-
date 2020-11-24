f96ce46b24f1c686361dd93062ee1855644abf61
Makov-Vik <72148650+Makov-Vik@users.noreply.github.com>
2020-11-24 09:59:29 +0200

    Update L -2.1.c

--- L -2.1/L -2.1.c
+++ L -2.1/L -2.1.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
 
 void main() {
-	int mat[5][5] = { {7, 3, 6, 8, 4},{6, 2, 5, 7, 3},{5, 1, 4, 6, 2}, {4, 0, 3, 5, 1}, {3, -1, 2, 4, 0} };
+	int mat[6][6] = { {7,  3,  6,  8,  4,  5},
+		          {6,  2,  5,  7,  3,  4},
+		          {5,  1,  4,  6,  2,  3}, 
+		          {4,  0,  3,  5,  1,  3}, 
+		          {3,  -1, 2,  4,  0,  1},
+			  {2,  -2, 1,  3,  -1,  0} };
 	
-	for (int i = 0; i < 5; i++) {
-		for (int j = 0; j < 5; j++) {
-			printf_s("%d ", mat[i][j]);
+	for (int i = 0; i < 6; i++) {
+		for (int j = 0; j < 6; j++) {
+			printf_s("%d  ", mat[i][j]);
 		}
 		printf_s("\n");
 	}
 	printf_s("\n");
 	printf_s("Search 4 \n");
 	printf_s("\n");
-	for (int j = 0; j < 5; j++) {
+	for (int j = 0; j < 6; j++) {
 		int search = 4;  //поиск этого элемента
 		int average = 0; //cредний элемент  
 		int first = 0;
-		int last = 4;
+		int last = 5;
 		while (first < last) {
 			average = first + (last - first) / 2;
 			if (search >= mat[average][j]) {
