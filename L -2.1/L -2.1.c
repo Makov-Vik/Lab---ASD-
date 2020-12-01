9edac1fb6ebe0007d62c827d778c85db7c1cd92e
Makov-Vik <72148650+Makov-Vik@users.noreply.github.com>
2020-12-01 12:44:06 +0200

    Update L -2.1.c

--- L -2.1/L -2.1.c
+++ L -2.1/L -2.1.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 void main() {
-	int mat[6][6] = { {7,  3,  6,  8,  4,  5},
-		          {6,  2,  5,  7,  3,  4},
-		          {5,  1,  4,  6,  2,  3}, 
-		          {4,  0,  3,  5,  1,  3}, 
+	int mat[6][6] = { {6,  3,  5,  7,  4,  5},
+			  {4,  2,  4,  6,  3,  4},
+			  {4,  1,  4,  5,  2,  3}, 
+		          {4,  0,  3,  4,  1,  3}, 
 		          {3,  -1, 2,  4,  0,  1},
 			  {2,  -2, 1,  3,  -1,  0} };
 	
@@ -15,8 +15,9 @@ void main() {
 		printf_s("\n");
 	}
 	printf_s("\n");
-	printf_s("Search 4 \n");
-	printf_s("\n");
+	printf_s("Search 4 \n \n");
+	printf_s("Program 1 \n");
+
 	for (int j = 0; j < 6; j++) {
 		int search = 4;  //поиск этого элемента
 		int average = 0; //cредний элемент  
@@ -35,6 +36,35 @@ void main() {
 		}
 		else
 			printf_s("value is not found in column  %d \n", j+1);
+		
+	}
 
+	printf_s("\nProgram 2 \n");
+	for (int j = 0; j < 6; j++) {
+		int search = 4;  //поиск этого элемента
+		int average = 0; //cредний элемент  
+		int first = 0;
+		int last = 5;
+		int peremena = 0;
+		while (first < last) {
+			average = first + (last - first) / 2;
+			if (search >= mat[average][j]) {
+				if (search == mat[average][j]) {
+					peremena = 1;
+					goto breakAll;
+				}
+				else
+					last = average;
+			}
+			else
+				first = average + 1;
+		}
+		
+	breakAll: {
+		if ( peremena == 0)
+			printf_s("value is not found in column  %d \n", j + 1);
+		else 
+			printf_s("Value is found in %d %d \n", average +1 , j + 1);
+		}		
 	}
 }
