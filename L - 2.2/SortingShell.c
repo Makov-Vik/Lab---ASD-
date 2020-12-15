f6c9e34b3404d97058f6ecdf5cb37b9ad5d70f23
Makov-Vik <72148650+Makov-Vik@users.noreply.github.com>
2020-12-16 00:32:15 +0200

    Update SortingShell.c

--- L - 2.2/SortingShell.c
+++ L - 2.2/SortingShell.c
@@ -13,4 +13,30 @@ int main(){
 		}
 		printf_s("\n");
 	}
+	
+	for (int i = 0; i < n; i++) {
+		int step = n / 2;
+		for (int j = 0; j < n + 1; j++) {
+			for (int count = 0; count < j ; count++) {
+				int index = count;
+				float value = mat[i][count];
+				while ((index >= step) && (mat[i][index - step] > value)){
+					mat[i][index] = mat[i][index - step];
+					index -= step;
+				}
+				mat[i][index] = value;
+			}
+			if (step > 1)
+				step /= 2;
+		}
+	}
+
+	printf_s("\n");
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			printf_s("%.2f ", mat[i][j]);
+		}
+		printf_s("\n");
+	}
 }
