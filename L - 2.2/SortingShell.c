61e0626b9f7aac8218f8c42778fa8006a01580b9
Makov-Vik <72148650+Makov-Vik@users.noreply.github.com>
2020-12-21 00:30:01 +0200

    Update SortingShell.c

--- L - 2.2/SortingShell.c
+++ L - 2.2/SortingShell.c
@@ -2,42 +2,51 @@
 #include <time.h>
 #include <conio.h>
 
-int main(){
-	
-	int n = 8;
-	float mat [8][8];
-
-	for (int i = 0; i < n; i++) { // Рандомное заполнение
-		for (int j = 0; j < n; j++) {
-			mat[i][j] = (100 +(rand()%1000))*0.01;
-			printf_s("%.2f ", mat[i][j]);
-		}
-		printf_s("\n");
-	}
-	
+void shell(float mat[8][8], int n /*, int i, int y*/) {
 	for (int i = 0; i < n; i++) { // Алгоритм Шелла
 		int step = n / 2;
 		for (int j = 0; j < n + 1; j++) {
-			for (int count = 0; count < j ; count++) {
+			for (int count = 0; count < j; count++) {
 				int index = count;
 				float value = mat[i][count];
-				while ((index >= step) && (mat[i][index - step] > value)){
+				while ((index >= step) && (mat[i][index - step] > value)) {
 					mat[i][index] = mat[i][index - step];
 					index -= step;
 				}
+
 				mat[i][index] = value;
 			}
 			if (step > 1)
 				step /= 2;
 		}
 	}
+}
+
+int main() {
+	float mat[8][8];
+	int n = 8;
+
+	for (int i = 0; i < n; i++) { // Рандомное заполнение
+		for (int j = 0; j < n; j++) {
+			mat[i][j] = (100 + (rand() % 1000)) * 0.01;
+			printf_s("%.2f ", mat[i][j]);
+		}
+		printf_s("\n");
+	}
+
+	shell(mat, n);
+	printf_s("%d", mat);
 
 	printf_s("\n");
 
-	for (int i = 0; i < n; i++) {
+	
+
+	for (int i = 0; i < n; i++) { // Вывод
 		for (int j = 0; j < n; j++) {
 			printf_s("%.2f ", mat[i][j]);
 		}
 		printf_s("\n");
 	}
+	return 0;
 }
+
